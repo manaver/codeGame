d3de36f363d35cbe890c97b07bf34ebcae6d75e8
Manav Verma <78682260+manaver@users.noreply.github.com>
2023-04-05 15:52:29 +0000

    level 5(2)

--- Level-5/code.py
+++ Level-5/code.py
@@ -24,15 +24,14 @@ class SHA256_hasher:
 
     # produces the password hash by combining password + salt because hashing
     def password_hash(self, password, salt):
-        password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
-        password_hash = bcrypt.hashpw(password, salt)
-        return password_hash.decode('ascii')
-
+        password = password.encode()
+        hashed = bcrypt.hashpw(password, salt)
+        return hashed.decode()
+    
     # verifies that the hashed password reverses to the plain text version on verification
     def password_verification(self, password, password_hash):
-        password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
-        password_hash = password_hash.encode('ascii')
-        return bcrypt.checkpw(password, password_hash)
+        return bcrypt.checkpw(password.encode(), password_hash.encode())
+
 
 class MD5_hasher:
     
